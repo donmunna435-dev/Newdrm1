91ec83ebe7e66168d7a312ec32e64337123457b0
donmunna435-dev <donmunna435@gmail.com>
2026-01-07 15:05:29 +0530

    Update bot configuration with new tokens and IDs
    Updated sensitive configuration values for the bot.

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
 import os
 
 class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    API_ID = int(os.environ.get("API_ID"))
-    API_HASH = os.environ.get("API_HASH")
-    AUTH_USER = os.environ.get('AUTH_USERS', '7602994049').split(',')
+    BOT_TOKEN = os.environ.get("8328284158:AAEklqmxjqcJgZhi6JYCpV7N1_qaxLZaIaM")
+    API_ID = int(os.environ.get("24754824"))
+    API_HASH = os.environ.get("e24a9c7a6aa24e1c56fa349e104ec20e")
+    AUTH_USER = os.environ.get('AUTH_USERS', '968292174').split(',')
     AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
     HOST = "https://drm-api-six.vercel.app"
     CREDIT = " 𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎"
+
